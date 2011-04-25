eb5ea0d69ad7263d4484c9ddd1510e171bc45c0d
Yanick Champoux <yanick@babyl.dyndns.org>
2011-04-24 20:58:09 -0400

    markdown source given in config file
    also, can set 'reload_presentation' to always
    rebuild the prez afresh. Very useful when writing
    the slides.

--- lib/chorus.pm
+++ lib/chorus.pm
@@ -10,20 +10,30 @@ use File::Slurp qw/ slurp /;
 
 our $VERSION = '0.1';
 
-my $prez;
-
 get '/' => sub {
     template 'index' => { 
-        presentation => $prez,
+        presentation => presentation(),
         prez_url => request->base,
         base_url => request->base->opaque,
+        aria => params->{aria},
     };
 };
 
+sub presentation {
+    state $presentation;
+
+    if ( not($presentation) or config->{reload_presentation} ) {
+        $presentation = load_presentation();
+    }
+
+    return $presentation;
+}
+
 sub load_presentation {
-    $prez = "<div class='slide'>". markdown( scalar slurp shift ) . "</div>";
+    my $prez = "<div class='slide'>". markdown( scalar slurp config->{presentation} ) . "</div>";
     my $heads;
     $prez =~ s#(?=<h1>)# $heads++ ? "</div><div class='slide'>" : "" #eg;
+    return $prez;
 }
 
 true;
